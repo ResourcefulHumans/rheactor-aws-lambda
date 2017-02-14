495c123c2060f3977fd8130e673e26a516f89eb5
Markus Tacker <markus@resourceful-humans.com>
2017-02-14 09:54:01 +0100

    feat(package): extracted from starhs-api-proxy-aws-lambda

---
+++ README.md
@@ -0,0 +1,11 @@
+# rheactor-aws-lambda
+
+[![npm version](https://img.shields.io/npm/v/@resourcefulhumans/rheactor-aws-lambda.svg)](https://www.npmjs.com/package/@resourcefulhumans/rheactor-aws-lambda)
+[![Build Status](https://travis-ci.org/ResourcefulHumans/rheactor-aws-lambda.svg?branch=master)](https://travis-ci.org/ResourcefulHumans/rheactor-aws-lambda)
+[![monitored by greenkeeper.io](https://img.shields.io/badge/greenkeeper.io-monitored-brightgreen.svg)](http://greenkeeper.io/) 
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+[![semantic-release](https://img.shields.io/badge/semver-semantic%20release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+[![Test Coverage](https://codeclimate.com/github/ResourcefulHumans/rheactor-aws-lambda/badges/coverage.svg)](https://codeclimate.com/github/ResourcefulHumans/rheactor-aws-lambda/coverage)
+[![Code Climate](https://codeclimate.com/github/ResourcefulHumans/rheactor-aws-lambda/badges/gpa.svg)](https://codeclimate.com/github/ResourcefulHumans/rheactor-aws-lambda)
+
+Core components for RESTful AWS lambda endpoints.
---
+++ test/handler.spec.js
@@ -0,0 +1,137 @@
+/* global describe, it */
+
+import {expect} from 'chai'
+import {handler, apiIndexOperation, statusOperation} from '../src'
+import {HttpProblem, Status} from 'rheactor-models'
+import {URIValue} from 'rheactor-value-objects'
+
+const contentType = 'application/vnd.resourceful-humans.rheactor-aws-lambda.v1+json'
+const environment = 'testing'
+const tokenSecretOrPrivateKey = 'foo'
+const headers = {'Content-type': contentType}
+const mountURL = new URIValue('https://api.example.com/')
+const operations = {
+  index: apiIndexOperation(mountURL, {
+    'status': Status.$context,
+    'empty': Status.$context
+  }),
+  status: statusOperation('0.0.0', environment, Date.now()),
+  empty: {
+    post: () => undefined
+  }
+}
+
+describe('index', () => {
+  it('should send not found if operation does not exist', done => {
+    const path = '/some/operation'
+    handler(
+      contentType,
+      environment,
+      tokenSecretOrPrivateKey,
+      operations,
+      {
+        headers,
+        path
+      }, null, (err, res) => {
+        expect(err).to.equal(null)
+        expect(res.statusCode).to.equal(404)
+        expect(res.headers).to.deep.equal({
+          'Content-Type': contentType,
+          'Access-Control-Allow-Origin': '*'
+        })
+        const expectedProblem = new HttpProblem(new URIValue('https://github.com/ResourcefulHumans/rheactor-aws-lambda#Error'), 'Unknown operation "/some/operation"', 404)
+        const body = JSON.parse(res.body)
+        const sentProblem = HttpProblem.fromJSON(body)
+        expect(sentProblem.name).to.equal(expectedProblem.name)
+        expect(sentProblem.type.equals(expectedProblem.type)).to.equal(true)
+        expect(sentProblem.title).to.equal(expectedProblem.title)
+        expect(sentProblem.$context).to.equal(expectedProblem.$context)
+        done()
+      })
+  })
+
+  it('should send bad request if operation does not support method', done => {
+    const path = '/status'
+    const httpMethod = 'DELETE'
+    const body = JSON.stringify({})
+    handler(
+      contentType,
+      environment,
+      tokenSecretOrPrivateKey,
+      operations,
+      {
+        headers,
+        httpMethod,
+        path,
+        body
+      }, null, (err, res) => {
+        expect(err).to.equal(null)
+        expect(res.statusCode).to.equal(400)
+        expect(res.headers).to.deep.equal({
+          'Content-Type': contentType,
+          'Access-Control-Allow-Origin': '*'
+        })
+        const expectedProblem = new HttpProblem(new URIValue('https://github.com/ResourcefulHumans/rheactor-aws-lambda#Error'), 'Unsupported action "DELETE /status"', 400)
+        const body = JSON.parse(res.body)
+        const sentProblem = HttpProblem.fromJSON(body)
+        expect(sentProblem.name).to.equal(expectedProblem.name)
+        expect(sentProblem.type.equals(expectedProblem.type)).to.equal(true)
+        expect(sentProblem.title).to.equal(expectedProblem.title)
+        expect(sentProblem.$context).to.equal(expectedProblem.$context)
+        done()
+      })
+  })
+
+  it('should send bad request if bad body provided', done => {
+    const path = '/status'
+    const httpMethod = 'POST'
+    const body = '{bad json'
+    handler(
+      contentType,
+      environment,
+      tokenSecretOrPrivateKey,
+      operations,
+      {
+        headers,
+        httpMethod,
+        path,
+        body
+      }, null, (err, res) => {
+        expect(err).to.equal(null)
+        expect(res.statusCode).to.equal(400)
+        expect(res.headers).to.deep.equal({
+          'Content-Type': contentType,
+          'Access-Control-Allow-Origin': '*'
+        })
+        const expectedProblem = new HttpProblem(new URIValue('https://github.com/ResourcefulHumans/rheactor-aws-lambda#SyntaxError'), 'Unexpected token b in JSON at position 1', 400)
+        const body = JSON.parse(res.body)
+        const sentProblem = HttpProblem.fromJSON(body)
+        expect(sentProblem.name).to.equal(expectedProblem.name)
+        expect(sentProblem.type.equals(expectedProblem.type)).to.equal(true)
+        expect(sentProblem.title).to.equal(expectedProblem.title)
+        expect(sentProblem.$context).to.equal(expectedProblem.$context)
+        done()
+      })
+  })
+
+  it('should return HTTP 204 if no response data is created', done => {
+    const path = '/empty'
+    const httpMethod = 'POST'
+    const body = ''
+    handler(
+      contentType,
+      environment,
+      tokenSecretOrPrivateKey,
+      operations,
+      {
+        headers,
+        httpMethod,
+        path,
+        body
+      }, null, (err, res) => {
+        expect(err).to.equal(null)
+        expect(res.statusCode).to.equal(204)
+        done()
+      })
+  })
+})
